8400ab9b350e0c7dfc42987dc372b02e96393e4e
aviv <avivabachi@gmail.com>
2022-08-23 15:44:50 +0300

    add comment model

---
+++ PetShopWebApp/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace PetShopWebApp.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public Animal? Animal { get; set; }
+        public string? Text { get; set; }
+        public string? Auther { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
