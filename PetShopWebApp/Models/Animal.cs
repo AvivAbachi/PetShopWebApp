f90b174f2f46929cce92a4f5569ba09e4156a091
Your Name <you@example.com>
2022-08-23 14:54:31 +0300

    Add user,animal and folder

---
+++ PetShopWebApp/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace PetShopWebApp.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? PictureURL { get; set; }
+        public string? Description { get; set; }
+        public Category? Category { get; set; }
+    }
+}
