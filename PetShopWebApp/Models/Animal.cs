1732d4e676d8e8c2d8a700080b791d431f9d2b32
aviv <avivabachi@gmail.com>
2022-08-23 14:56:54 +0300

    age to flot

--- PetShopWebApp/Models/Animal.cs
+++ PetShopWebApp/Models/Animal.cs
@@ -4,7 +4,7 @@
     {
         public int AnimalId { get; set; }
         public string? Name { get; set; }
-        public int Age { get; set; }
+        public float Age { get; set; }
         public string? PictureURL { get; set; }
         public string? Description { get; set; }
         public Category? Category { get; set; }
