82086d0cb5e98aca7b4d21964f52df544332e731
aviv <avivabachi@gmail.com>
2022-08-23 15:55:39 +0300

    integrate db

---
+++ PetShopWebApp/Data/PetShopConetex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShopWebApp.Models;
+
+namespace PetShopWebApp.Data
+{
+    public class PetShopConetex : DbContext
+    {
+        public PetShopConetex(DbContextOptions<PetShopConetex> options) : base(options) { }
+
+        public DbSet<Animal>? Animals { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+        public DbSet<Category>? Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- PetShopWebApp/Program.cs
+++ PetShopWebApp/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using PetShopWebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+builder.Services.AddDbContext<PetShopConetex>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<PetShopConetex>();
+    ctx.Database.EnsureDeleted();
+    ctx.Database.EnsureCreated();
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
