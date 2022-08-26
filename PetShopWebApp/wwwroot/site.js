accae33970fe906fd95d0fb20ebd55272cff3a82
aviv <avivabachi@gmail.com>
2022-08-26 11:15:35 +0300

    add site js and css

---
+++ PetShopWebApp/wwwroot/site.js
@@ -0,0 +1,8 @@
+﻿// Function Add Like
+$('.btn-like').click(
+    function (btn) {
+        const id = btn.currentTarget.dataset.animal;
+        $.post("/Home/AddAnimalLike/" + id, function (data) { $("#like_" + id).html(data); });
+        btn.currentTarget.setAttribute('disabled', '');
+    }
+)
